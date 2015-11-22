2fc4208ac09d422de3904496d1fad54d93ce5f87
Benjamin Schaaf <ben.schaaf@gmail.com>
2015-11-22 17:41:22 +1100

    Cleaned up image interfaces

--- source/dil/image.d
+++ source/dil/image.d
@@ -2,16 +2,23 @@ module dil.image;
 
 import std.typecons;
 
-interface Image(PixelFmt) {
-    PixelFmt opIndex(size_t x, size_t y);
-    PixelFmt opIndexAssign(PixelFmt pixel, size_t x, size_t y);
+import dil.pixels;
 
+/// Interface for all images
+interface Image {
     @property size_t width();
     @property size_t height();
 
     final @property auto size() {
         return tuple(width, height);
     }
+}
+
+/// Interface for specific pixel formats. Abstracted away by Image.
+interface ImageManip(PixelFmt) : Image {
+    PixelFmt opIndex(size_t x, size_t y);
+    PixelFmt opIndexAssign(PixelFmt pixel, size_t x, size_t y);
+
     final size_t opDollar(size_t pos)() if (pos == 0) {
         return width;
     }
