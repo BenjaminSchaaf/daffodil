0f61b1d4fd9e1d61d32036405bbdc736893faabe
BenjaminSchaaf <ben.schaaf@gmail.com>
2015-12-23 18:22:19 +1100

    Added myself to copyright

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'DIL'
-copyright = '2015, Cameron Lonsdale'
+copyright = '2015, Cameron Lonsdale, Benjamin Schaaf'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
