e83cb38e8e77b563044046611e34e4a507f001d4
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-04-15 14:08:31 +1000

    Made docs show todo items

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ extensions = [
 ]
 
 highlight_language = 'd'
+todo_include_todos = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
