46e4ab07ab28bd33ff4d7a9bf6553dcfd3e3dd75
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-07-13 23:25:17 +0200

    Made fields of the ColorSpace struct documented

--- source/daffodil/colorspace.d
+++ source/daffodil/colorspace.d
@@ -8,8 +8,11 @@ import std.algorithm;
  * A group of functions describing the operations for a :d:struct:`Pixel`.
  */
 struct ColorSpace {
+    ///
     void function(const real[], const real, real[]) opScalarMul;
+    ///
     void function(const real[], const real[], real[]) opColorAdd;
+    ///
     string function(const real[]) toString;
 }
 
